1225c5aff30b6526eeb2d8ae3293280ac94abd80
Gyani25k <kgyanender4@gmail.com>
2025-02-07 00:49:16 +0530

    Implement data ingestion configuration and utility functions

---
+++ src/ds_e2e_project_1/constants/__init__.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+PARAM_FILE_PATH = Path("params.yaml")
+CONFIG_FILE_PATH = Path("config/config.yaml")
+SCHEMA_FILE_PATH = Path("schema.yaml")
+
+print(CONFIG_FILE_PATH,PARAM_FILE_PATH,SCHEMA_FILE_PATH)
---
+++ src/ds_e2e_project_1/utils/common.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from box import ConfigBox 
+import yaml
+import os
+from src.ds_e2e_project_1 import logger
+from ensure import ensure_annotations
+
+def read_yaml(path_to_yaml:Path) -> ConfigBox:
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file:{path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except Exception as e:
+        raise ValueError("Yaml Fle is empy")
+
+@ensure_annotations
+def create_directories(path_to_directories:list , verbose = True):
+    for path in path_to_directories:
+        os.makedirs(path,exist_ok=True)
+        if verbose:
+            logger.info(f"created directory at path: {path}")
