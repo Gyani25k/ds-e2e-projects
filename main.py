10c3d51a0617ed93205e626ded7f5775b767abb2
Gyani25k <kgyanender4@gmail.com>
2025-02-18 00:34:09 +0530

    Refactor data ingestion process: implement new pipeline class and update main script

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 from src.ds_e2e_project_1 import logger
-logger.info("Hello World")
+from src.ds_e2e_project_1.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.initiate_data_ingestion()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<\n\nx=========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
